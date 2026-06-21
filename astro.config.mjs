c0ee22161883180a550a917d173003cf9e94f783
Mohamed-lamin Taleb <daminsoft@gmail.com>
2026-06-21 11:52:16 +0200

    pdfsenior first commit 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: node({ mode: 'standalone' }),
   integrations: [
     react(),
     tailwind(),
